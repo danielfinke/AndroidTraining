003cbb21f3ea02426b481ba5cdd72aaf24d2f723
Daniel Finke <danielfinke2011@gmail.com>
2016-08-29 17:02:07 -0700

    Animation control with lifecycle

--- app/src/main/java/com/finke/androidtraining/DisplayMessageActivity.java
+++ app/src/main/java/com/finke/androidtraining/DisplayMessageActivity.java
@@ -1,13 +1,21 @@
 package com.finke.androidtraining;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    private final static String TAG = DisplayMessageActivity.class.getName();
+
+    private ImageView mSoccerBall;
+    private boolean mSoccerBallAnimating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +32,72 @@ public class DisplayMessageActivity extends AppCompatActivity {
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         textView.setLayoutParams(params);
         layout.addView(textView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+
+        if(mSoccerBall == null) {
+            mSoccerBall = (ImageView)findViewById(R.id.soccerBallImage);
+        }
+        mSoccerBallAnimating = true;
+        bounceRight();
+    }
+
+    private void bounceRight() {
+        if(!mSoccerBallAnimating) { return; }
+        mSoccerBall.post(new Runnable() {
+            @Override
+            public void run() {
+                ViewGroup parent = (ViewGroup)mSoccerBall.getParent();
+                mSoccerBall.animate()
+                        .x(parent.getWidth() - parent.getPaddingLeft() - mSoccerBall.getPaddingLeft() - mSoccerBall.getWidth())
+                        .setDuration(1000)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                bounceLeft();
+                            }
+                        });
+            }
+        });
+    }
+
+    private void bounceLeft() {
+        if(!mSoccerBallAnimating) { return; }
+        mSoccerBall.post(new Runnable() {
+            @Override
+            public void run() {
+                ViewGroup parent = (ViewGroup)mSoccerBall.getParent();
+                mSoccerBall.animate()
+                        .x(parent.getPaddingLeft())
+                        .setDuration(1000)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                bounceRight();
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+
+        mSoccerBallAnimating = false;
+        mSoccerBall.clearAnimation();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
