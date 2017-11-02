a3a89ddca8c31b42fd109d5ed2e430e9790b40e8
Daniel Finke <danielfinke2011@gmail.com>
2017-11-02 14:15:40 -0700

    Dynamic vs static fragments

--- app/src/main/java/com/finke/androidtraining/MainActivity.java
+++ app/src/main/java/com/finke/androidtraining/MainActivity.java
@@ -6,16 +6,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import com.finke.androidtraining.fragments.DynamicFragment;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getName();
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private RelativeLayout mFragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
+
+        mFragmentContainer = (RelativeLayout)findViewById(R.id.fragment_container);
+
+        if(savedInstanceState != null) {
+            return;
+        }
+
+        DynamicFragment dynamicFragment = new DynamicFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, dynamicFragment).commit();
     }
 
     @Override
---
+++ app/src/main/java/com/finke/androidtraining/fragments/StaticFragment.java
@@ -0,0 +1,22 @@
+package com.finke.androidtraining.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.finke.androidtraining.R;
+
+/**
+ * @since 2017-03-23
+ */
+
+public class StaticFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.static_fragment, container, false);
+    }
+}
